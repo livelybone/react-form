085a9c98dae972d700b9a48a33b99ae6c2f75efd
livelybone <liuzhiping@auraxy.com>
2019-11-13 11:06:32 +0800

    feat: Update form

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,12 @@
 import {
-  FormItem,
-  FormOptions,
-  FormItemsData,
   Form,
+  FormItem,
+  FormItemsData,
+  FormOptions,
   TupleToUnion,
 } from '@livelybone/form'
 import { ChangeEvent } from 'react'
+
 export * from '@livelybone/form'
 
 /**
@@ -21,10 +22,12 @@ declare function useForm<
     ReturnTypeOfSubmit | FormItemsData<Items>
   >,
 ): Form<Items, ReturnTypeOfSubmit>
+
 declare function isAllItemFilled<
   FormItems extends FormItem<any, any, any>[],
   Options extends FormOptions<any, any>
 >(form: Form<FormItems, Options>): boolean
+
 declare function inputItemChange<
   FormItems extends FormItem<any, any, any>[],
   Options extends FormOptions<any, any>,
